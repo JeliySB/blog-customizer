9518f8dcf325ae1e4bbf359f8d87a3b791c4d1ad
JeliySB <theworst03@yandex.ru>
2026-02-25 17:24:22 +0800

    Добавлен сайдбар, работает

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,15 +3,148 @@ import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { useRef, useEffect, useState } from 'react';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from '../../constants/articleProps';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+
+import clsx from 'clsx';
+
+interface ArticleParamsFormProps {
+	isOpen: boolean;
+	onClose: () => void;
+	onApply: (values: ArticleStateType) => void;
+	onReset: () => void;
+}
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onClose,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const sidebarRef = useRef<HTMLElement>(null);
+
+	const [formValues, setFormValues] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// закрытие
+	useEffect(() => {
+		if (!isOpen) return;
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				onClose();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [isOpen, onClose]);
+
+	const handleReset = () => {
+		setFormValues(defaultArticleState);
+		onReset();
+	};
+
+	const handleApply = () => {
+		onApply(formValues);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<div className={styles.header}>
+					<ArrowButton isOpen={true} onClick={onClose} />
+				</div>
+
 				<form className={styles.form}>
+					{/* Заголовок */}
+					<Text as='h2' size={31} weight={800} uppercase>
+						Задать параметры
+					</Text>
+
+					{/* Шрифт */}
+					<Select
+						title='ШРИФТ'
+						selected={formValues.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) =>
+							setFormValues((prev) => ({ ...prev, fontFamilyOption: option }))
+						}
+					/>
+
+					{/* размер шрифта */}
+					<RadioGroup
+						name='font-size'
+						title='РАЗМЕР ШРИФТА'
+						options={fontSizeOptions}
+						selected={formValues.fontSizeOption}
+						onChange={(option) =>
+							setFormValues((prev) => ({ ...prev, fontSizeOption: option }))
+						}
+					/>
+
+					{/* цвет шрифта */}
+					<Select
+						title='ЦВЕТ ШРИФТА'
+						selected={formValues.fontColor}
+						options={fontColors}
+						onChange={(option) =>
+							setFormValues((prev) => ({ ...prev, fontColor: option }))
+						}
+					/>
+
+					<Separator />
+
+					{/* цвет фона */}
+					<Select
+						title='ЦВЕТ ФОНА'
+						selected={formValues.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) =>
+							setFormValues((prev) => ({ ...prev, backgroundColor: option }))
+						}
+					/>
+
+					{/* ширина контента */}
+					<Select
+						title='ШИРИНА КОНТЕНТА'
+						selected={formValues.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) =>
+							setFormValues((prev) => ({ ...prev, contentWidth: option }))
+						}
+					/>
+
+					{/* кнопки */}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={handleApply}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -9,23 +9,39 @@ import { defaultArticleState } from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
+import { ArrowButton } from './ui/arrow-button';
+
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false); // состояние
+	const [appliedState, setAppliedState] = useState(defaultArticleState);
+
+	const toggle = () => setIsOpen((prev) => !prev);
+
+	const appliedStyles = {
+		'--font-family': appliedState.fontFamilyOption.value,
+		'--font-size': appliedState.fontSizeOption.value,
+		'--font-color': appliedState.fontColor.value,
+		'--container-width': appliedState.contentWidth.value,
+		'--bg-color': appliedState.backgroundColor.value,
+	} as CSSProperties;
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<main className={clsx(styles.main)} style={appliedStyles}>
+			{!isOpen && (
+				<div className={styles.arrowButtonWrapper}>
+					<ArrowButton isOpen={false} onClick={toggle} />
+				</div>
+			)}
+
+			<ArticleParamsForm
+				isOpen={isOpen}
+				onClose={toggle}
+				onApply={(values) => setAppliedState(values)}
+				onReset={() => setAppliedState(defaultArticleState)}
+			/>
 			<Article />
 		</main>
 	);
